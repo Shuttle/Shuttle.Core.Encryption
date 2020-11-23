7fb2554f69ffdd519cbc372225838e0e2fd8ee90
Gábor Zavarkó <gabor.zavarko@horizonte.as>
2020-11-23 12:29:13 +0100

    targetframework update

--- Shuttle.Core.Encryption/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Encryption/Properties/AssemblyInfo.cs
@@ -1,22 +1,18 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NET461
-[assembly: AssemblyTitle(".NET Framework 4.6.1")]
-#endif
-
-#if NETCOREAPP2_1
-[assembly: AssemblyTitle(".NET Core 2.1")]
-#endif
-
 #if NETSTANDARD2_0
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.2.0")]
+#if NETSTANDARD2_1
+[assembly: AssemblyTitle(".NET Standard 2.1")]
+#endif
+
+[assembly: AssemblyVersion("10.0.3.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
 [assembly: AssemblyProduct("Shuttle.Core.Encryption")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.2")]
+[assembly: AssemblyInformationalVersion("10.0.3")]
 [assembly: ComVisible(false)]
